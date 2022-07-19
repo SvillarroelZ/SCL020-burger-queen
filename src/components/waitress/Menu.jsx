70f30116ca4f15c988f436270acc5a244a255652
Constanza <c.leytonleiva@gmail.com>
2022-07-19 18:14:47 -0400

    waitress

--- src/components/waitress/Menu.jsx
+++ src/components/waitress/Menu.jsx
@@ -1,13 +1,46 @@
+import ReactDOM from 'react-dom/client'
 import {useState, useContext} from "react";
 import { MenuContext } from "./Pedido";
 
- export const MenuOpt export const MenuOpt = ({data}) => {
+const Menu = ({data}) => {
     const context = useContext(MenuContext);
-    const[product, setProduct] = useState([]);
-
-    const productType = (options) => {
+    const[Menu, setMenu] = useState([]);
+    const menuHrs = (options) => {
         switch (options){
-            case
+            case "day":
+                const dayOp = data.filter((elem)=> elem.menuOptions === options);
+                setMenu(dayOp);
+                
+                break;
+            
+            case "night":
+                const nightOp = data.filter((elem)=> elem.menu === options);
+                setMenu(nightOp)
+                break;
         }
     }
+    return (
+        <div>
+            <div>
+                <ul>
+                    <button onClick={() => menuHrs("day")}>sol</button>
+                    <button onClick={() => menuHrs("night")}>luna</button>
+                </ul>
+            </div>  
+            <div className="menuItems">
+                {Menu.map((item, index)=>(
+                <div key={index} className="itemCont">
+                    <img src={item.img} alt="" />
+                    <p className="title-items">${item.name}</p>
+                    <h2 className="price">${item.price}</h2>
+                    <button onClick={() => context.addProduct(item)} className="btn-add"></button>
+                </div>
+                ))}
+            </div>
+        </div>
+    )
 }
+
+export default Menu;
+
+
--- src/components/waitress/Pedido.jsx
+++ src/components/waitress/Pedido.jsx
@@ -1,9 +1,18 @@
 import { useState, createContext } from "react";
-import menudataJson from'../../menudata.json'
+import menudataJson from'../../menudata.json';
+import Menu from "./Menu";
 
-export const MenuContext = createContext();
+
+export const MenuContext = createContext(menudataJson);
 
 export const WaitressHome = () => {
-    const data = dataJson.menu;
+    const data = menudataJson.menuOptions;
+    const [state, setState] = useState({
+        menuList: data,
+        cart: []
+    });
+
+
 
 }
+
